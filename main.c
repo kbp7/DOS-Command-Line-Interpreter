199fcf8b63c41d616fcdbe8fff2b9564f3d17ea8
Kevin Poon <kbp7@hotmail.com>
2017-10-03 16:10:07 -0400

    Implemented conversion from DOS to UNIX. Needs testing on server.

--- main.c
+++ main.c
@@ -4,24 +4,43 @@
 
 int main ()
 {
-    char input[50];
+    char input[64];
+    char *command;
+    char *arg1;
+    char *arg2;
+    char *arg3;
     while(1) {
         printf("Enter DOS input: ");
         fgets(input, sizeof(input), stdin);
         printf("ECHO: %s", &input);
+        input[strlen(input) - 1] = '\0';
+        command = strtok(input, " ");
+        arg1 = strtok(NULL, " ");
+        arg2 = strtok(NULL, " ");
+        arg3 = strtok(NULL, " "); //should always be null
 
-        char *command = strtok(input, " ");
-        char *arg1 = strtok(NULL, " ");
-        char *arg2 = strtok(NULL, " ");
-        char *arg3 = strtok(NULL, " "); //should always be null
+        printf("Command: %s\n", command);
+        printf("Command: %s\n", arg1);
+        printf("Command: %s\n", arg2);
+        printf("Command: %s\n", arg3);
         // Keep printing commands while one of the
         // delimiters present in str[].
         //while (command != NULL)
         //{
 
                 //1 arg
-            if(strcmp(command, "cd")) {
-                if(arg1 != NULL && arg2 == NULL) {
+                //cd foldername
+            if(strcmp(input, "cd") == 0) {
+                if(arg1 == NULL) {
+                    printf("0 args\n");
+                    system("cd");
+                }
+                else if(arg2 == NULL) {
+                    printf("1 arg\n");
+                    char out[64];
+                    strcpy(out, "cd ");
+                    strcat(out, arg1);
+                    system(out);
                     system(input);
                 }
                 else {
@@ -29,39 +48,69 @@ int main ()
                 }
             }
                 //0 or 1 arg
-            else if(strcmp(command, "dir")) {
+            else if(strcmp(input, "dir") == 0) {
                 if(arg1 == NULL) {
+                    printf("0 args\n");
                     system("ls");
                 }
                 else if(arg2 == NULL) {
-
+                    printf("1 arg\n");
+                    char out[64];
+                    strcpy(out, "ls ");
+                    strcat(out, arg1);
+                    system(out);
                 }
                 else {
                     printf("Invalid input!");
                 }
             }
+
                 //1 or 2 args
-            else if(strcmp(command, "type")) {
-                if(arg1 != NULL && arg3 == NULL) {
-                    system(input);
+                //cat [OPTION]... [FILE]...
+            else if(strcmp(input, "type")) {
+                if(arg1 != NULL && arg2 == NULL) {
+                    printf("1 arg\n");
+                    char out[64];
+                    strcpy(out, "cat ");
+                    strcat(out, arg1);
+                    system(out);
+                }
+                else if(arg2 != NULL && arg3 == NULL) {
+                    printf("2 args\n");
+                    char out[64];
+                    strcpy(out, "cat ");
+                    strcat(out, arg1);
+                    strcat(out, arg2);
+                    system(out);
                 }
                 else {
                     printf("Invalid input!");
                 }
             }
                 //1 arg
+                //rm FILE...
             else if(strcmp(command, "del")) {
-                if(arg1 != NULL) {
-                    system(input);
+                if(arg1 != NULL && arg2 == NULL) {
+                    printf("1 arg\n");
+                    char out[64];
+                    strcpy(out, "cat ");
+                    strcat(out, arg1);
+                    system(out);
                 }
                 else {
                     printf("Invalid input!");
                 }
             }
                 //2 args
+                //rename directory1 directory2
             else if(strcmp(command, "ren")) {
                 if((arg1 != NULL && arg2 != NULL) && arg3 == NULL ) {
-                    system(input);
+                    printf("2 args\n");
+                    char out[64];
+                    strcpy(out, "mv ");
+                    strcat(out, arg1);
+                    strcat(out, arg2);
+                    system(out);
                 }
                 else {
                     printf("Invalid input!");
@@ -70,7 +119,12 @@ int main ()
                 //2 args
             else if(strcmp(command, "copy")) {
                 if((arg1 != NULL && arg2 != NULL) && arg3 == NULL ) {
-                    system(input);
+                    printf("2 args\n");
+                    char out[64];
+                    strcpy(out, "cp ");
+                    strcat(out, arg1);
+                    strcat(out, arg2);
+                    system(out);
                 }
                 else {
                     printf("Invalid input!");
@@ -79,7 +133,7 @@ int main ()
 
 
         printf("Press CTRL-C to exit\n");
-        input[strlen(input) - 1] = '\0';
+
 
         }
     //}
